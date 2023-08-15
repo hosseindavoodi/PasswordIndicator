0970254333b7ac802e28d3430c4d46eb0b3d7062
Hossein Davoodi <H.Davoodi@trenkwalder.com>
2023-08-15 16:57:00 +0300

    Fix a bug related to checking characters

--- utils/passwordIndicator.ts
+++ utils/passwordIndicator.ts
@@ -1,46 +1,43 @@
 export const passwordIndicator = (param: string) => {
-    let passwordCheck = 0
-    let validateRegex = ['[A-Z]', '[a-z]', '[0-9]', '\\W']
-    validateRegex.forEach(regex => {
-      if (new RegExp(regex).test(param)) {
-        passwordCheck += 1
-      }
-    })
-      const str = param && param.replace(/\s+/g, '_')
-    if (/(\S)(\1{3,})/g.test(str)) {
-      passwordCheck -= 1
+  let passwordCheck = 0;
+  let validateRegex = ["[A-Z]", "[a-z]", "[0-9]", "\\W"];
+  validateRegex.forEach((regex) => {
+    if (param && param.match(regex)) {
+      passwordCheck += 1;
     }
-    
-    if (passwordCheck > 2 && param.length > 5) {
-      passwordCheck += 1
-    }
-    if (passwordCheck > 3 && param.length > 4) {
-      passwordCheck += 1
-    }
-    if (passwordCheck > 3 && param.length > 7) {
-      passwordCheck += 1
-    }
-    if (passwordCheck > 4 && param.length > 9) {
-      passwordCheck += 1
-    }
-    if (passwordCheck > 5 && param.length > 11) {
-      passwordCheck += 1
-    }
-  
-    return passwordCheck
+  });
+  const str = param && param.replace(/\s+/g, "_");
+  if (/(\S)(\1{3,})/g.test(str)) {
+    passwordCheck -= 1;
   }
 
-
-  export const hasMinCharacters = (param: string) => {
-    return param && param.length > 5
+  if (passwordCheck > 2 && param.length > 5) {
+    passwordCheck += 1;
+  }
+  if (passwordCheck > 3 && param.length > 4) {
+    passwordCheck += 1;
+  }
+  if (passwordCheck > 3 && param.length > 7) {
+    passwordCheck += 1;
+  }
+  if (passwordCheck > 4 && param.length > 9) {
+    passwordCheck += 1;
+  }
+  if (passwordCheck > 5 && param.length > 11) {
+    passwordCheck += 1;
   }
 
-  export const hasNumber = (param: string) => {
-    return /[0-9]/.test(param)
-  }
-  
-  export const hasUppercaseLetter = (param: string) => {
-    return /[A-Z]/.test(param)
-  }
+  return passwordCheck;
+};
 
-  
+export const hasMinCharacters = (param: string) => {
+  return param && param.length > 5;
+};
+
+export const hasNumber = (param: string) => {
+  return /[0-9]/.test(param);
+};
+
+export const hasUppercaseLetter = (param: string) => {
+  return /[A-Z]/.test(param);
+};
